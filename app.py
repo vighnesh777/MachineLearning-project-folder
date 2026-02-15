0a4b55b0521d30adf8b6674b489eb92f6569dfbe
Vighnesh Kanakala <63389698+vighnesh777@users.noreply.github.com>
2026-02-15 18:47:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,68 +52,73 @@ def processing_data(file):
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.transform(X_test)
         return X_train ,X_test, y_train, y_test , None
+    except FileNotFoundError:
+        return None,None,None,None,"Default file 'diabetes_data_upload.csv' not foud"
     except Exception as e:
         return None, None,None,None, str(e)
-    
+
+data_source = ""
 if uploaded_file is not None:
-    X_train , X_test, y_train , y_test, error = processing_data(uploaded_file)
-    if error:
-        sl.error(error)
-    else:
-        sl.success("Data is loaded now, and pre processed")
-
-        if model_name == "Log Regression":
-            model = LogisticRegression(random_state=42, max_iter=5000)
-        elif model_name == "Descision Tree":
-            model = DecisionTreeClassifier(random_state=42, criterion='entropy')
-        elif model_name == "K-th Nearest Neighbours":
-            k = k_neighbors
-            model = KNeighborsClassifier(n_neighbors=k)
-        elif model_name == "Naive Bayes":
-            model = GaussianNB()
-        elif model_name == "Random Forest":
-            est = n_estimators
-            model = RandomForestClassifier(n_estimators=est,random_state=42)
-        elif model_name == "XGBoost" :
-            model = XGBClassifier(n_estimators=100,learning_rate=lr,random_state=42,use_label_encoder=False,eval_metric='logloss')
-        
-        if sl.button("Evaluate") :
-            with sl.spinner(f"Training {model_name}"):
-                model.fit(X_train,y_train)
-                y_pred = model.predict(X_test)
-                try:
-                    y_prob = model.predict_proba(X_test)[:,1]
-                except:
-                    y_prob = None
-            sl.divider()
-            sl.subheader(f"Results / Performance for {model_name}")
-            accuracy = accuracy_score(y_test,y_pred)
-            precision = precision_score(y_test,y_pred,zero_division=0)
-            recall = recall_score(y_test,y_pred,zero_division=0)
-            f1 = f1_score(y_test,y_pred,zero_division=0)
-            mcc = matthews_corrcoef(y_test,y_pred)
-            try:
-                auc_score = roc_auc_score(y_test,y_prob) if y_prob is not None else roc_auc_score(y_test,y_pred)
-            except:
-                auc_score =0.0
-            m1,m2,m3 = sl.columns(3)
-            m4,m5,m6 = sl.columns(3)
-            m1.metric("Accuracy", f"{accuracy:.4f}")
-            m2.metric("Precision" , f"{precision:.4f}")
-            m3.metric("Recall" , f"{recall:.4f}")
-            m4.metric("F1 Score" , f"{f1:.4f}")
-            m6.metric("MCC Score" , f"{mcc:.4f}")
-            m5.metric("AUC Score" , f"{auc_score:.4f}")
-            sl.divider()
-            col_graph,col_text = sl.columns([1,1])
-            with col_graph:
-                sl.write("#### Confusion Matrix")
-                cm = confusion_matrix(y_test,y_pred)
-                fig,ax = mpl.subplots(figsize=(6,5))
-                sea.heatmap(cm,annot=True,fmt='d',cmap="Blues",ax=ax)
-                ax.set_ylabel('Actual Label')
-                ax.set_xlabel('Prediction Label')
-                sl.pyplot(fig)
+    data_source = uploaded_file
+    source_label = "Uploaded file"
 else:
-    sl.info("Please upload the CSV file to start")     
+    data_source = "diabetes_data_upload.csv"
+    source_label = "Default Dataset"
 
+X_train , X_test, y_train , y_test, error = processing_data(data_source)
+if error:
+    sl.error(error)
+else:
+    sl.success("Data is loaded now, and pre processed")
+
+    if model_name == "Log Regression":
+        model = LogisticRegression(random_state=42, max_iter=5000)
+    elif model_name == "Descision Tree":
+        model = DecisionTreeClassifier(random_state=42, criterion='entropy')
+    elif model_name == "K-th Nearest Neighbours":
+        k = k_neighbors
+        model = KNeighborsClassifier(n_neighbors=k)
+    elif model_name == "Naive Bayes":
+        model = GaussianNB()
+    elif model_name == "Random Forest":
+        est = n_estimators
+        model = RandomForestClassifier(n_estimators=est,random_state=42)
+    elif model_name == "XGBoost" :
+        model = XGBClassifier(n_estimators=100,learning_rate=lr,random_state=42,use_label_encoder=False,eval_metric='logloss')
+    
+    with sl.spinner(f"Training {model_name}"):
+        model.fit(X_train,y_train)
+        y_pred = model.predict(X_test)
+        try:
+            y_prob = model.predict_proba(X_test)[:,1]
+        except:
+            y_prob = None
+    sl.divider()
+    sl.subheader(f"Results / Performance for {model_name}")
+    accuracy = accuracy_score(y_test,y_pred)
+    precision = precision_score(y_test,y_pred,zero_division=0)
+    recall = recall_score(y_test,y_pred,zero_division=0)
+    f1 = f1_score(y_test,y_pred,zero_division=0)
+    mcc = matthews_corrcoef(y_test,y_pred)
+    try:
+        auc_score = roc_auc_score(y_test,y_prob) if y_prob is not None else roc_auc_score(y_test,y_pred)
+    except:
+        auc_score =0.0
+    m1,m2,m3 = sl.columns(3)
+    m4,m5,m6 = sl.columns(3)
+    m1.metric("Accuracy", f"{accuracy:.4f}")
+    m2.metric("Precision" , f"{precision:.4f}")
+    m3.metric("Recall" , f"{recall:.4f}")
+    m4.metric("F1 Score" , f"{f1:.4f}")
+    m6.metric("MCC Score" , f"{mcc:.4f}")
+    m5.metric("AUC Score" , f"{auc_score:.4f}")
+    sl.divider()
+    col_graph,col_text = sl.columns([1,1])
+    with col_graph:
+        sl.write("#### Confusion Matrix")
+        cm = confusion_matrix(y_test,y_pred)
+        fig,ax = mpl.subplots(figsize=(6,5))
+        sea.heatmap(cm,annot=True,fmt='d',cmap="Blues",ax=ax)
+        ax.set_ylabel('Actual Label')
+        ax.set_xlabel('Prediction Label')
+        sl.pyplot(fig)
